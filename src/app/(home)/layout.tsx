6faddb8e2665f8b57a4b6483c71aa71277f6a4c3
d3m0n <th3l1ghtd3m0n@gmail.com>
2024-11-24 15:15:11 -0300

    feat: add bars to layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { currentUser } from '@clerk/nextjs/server';
+import LeftSidebar from '@/components/LeftSidebar';
+import RightSidebar from '@/components/RightSidebar';
 
-const layout = ({ children }: { children: React.ReactNode }) => {
+const layout = async ({ children }: { children: React.ReactNode }) => {
+  const user = await currentUser();
+
+  if (!user) return null;
+
   return (
-    <main className="max-w-7xl mx-auto bg-red-500">
-      LeftSideBar
+    <main className="h-full max-w-7xl mx-auto bg-red-500">
+      <LeftSidebar username={user.username!} />
       {children}
-      RightSideBar
+      <RightSidebar />
     </main>
   );
 };
