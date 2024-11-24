19a30a0d7fdff1874d874da29dec84ae86f648ab
d3m0n <th3l1ghtd3m0n@gmail.com>
2024-11-24 07:44:59 -0300

    feat(api): add uploadthing api

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,36 @@
+import { currentUser } from '@clerk/nextjs/server';
+import { createUploadthing } from 'uploadthing/next';
+import type { FileRouter } from 'uploadthing/next';
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  imageProfile: f({
+    image: { maxFileSize: '1MB' }
+  })
+    .middleware(async () => {
+      const user = await currentUser();
+
+      if (!user) throw new Error('Unauthorized');
+
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ file }) => {
+      console.info('file url:', file.url);
+    }),
+  imageThreaad: f({
+    image: { maxFileSize: '2MB' }
+  })
+    .middleware(async () => {
+      const user = await currentUser();
+
+      if (!user) throw new Error('Unauthorized');
+
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ file }) => {
+      console.info('file url:', file.url);
+    })
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
