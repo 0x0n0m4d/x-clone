be8c4410ac93c679d498fa39287b918217aa9b25
d3m0n <th3l1ghtd3m0n@gmail.com>
2024-11-24 17:34:35 -0300

    style: change value

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <main className="min-h-screen grid place-items-center">{children}</main>
-  );
+  return <main className="h-screen grid place-items-center">{children}</main>;
 };
 
 export default layout;
