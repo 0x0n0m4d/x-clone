662af1de6254ffeab59ebf4a3665a504574ef566
d3m0n <th3l1ghtd3m0n@gmail.com>
2024-11-24 08:21:31 -0300

    feat: add landing page

---
+++ src/components/CreateAnAccount.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { Button } from './ui/button';
+
+const CreateAnAccount = () => {
+  const router = useRouter();
+  return (
+    <Button
+      variant="primary"
+      onClick={() => {
+        router.push('/sign-in');
+      }}
+    >
+      Create An Account
+    </Button>
+  );
+};
+
+export default CreateAnAccount;
