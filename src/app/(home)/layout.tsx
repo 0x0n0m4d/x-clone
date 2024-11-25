c44f02ea1ad43426721cd74bf6bea2e0601bc384
d3m0n <th3l1ghtd3m0n@gmail.com>
2024-11-25 04:48:26 -0300

    fix(component): fix LeftSidebar component on home layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+import { getUser } from '@/actions/user.action';
 import LeftSidebar from '@/components/LeftSidebar';
 import RightSidebar from '@/components/RightSidebar';
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
-  const user = await currentUser();
+  const clerkUser = await currentUser();
 
-  if (!user) return null;
+  if (!clerkUser) return null;
+
+  const user = await getUser(clerkUser.id);
+
+  const isCompleted = user && user.isCompleted;
+  if (!isCompleted) redirect('/onboarding');
 
   return (
-    <main className="h-full max-w-7xl mx-auto bg-red-500">
-      <LeftSidebar username={user.username!} />
+    <main className="h-full max-w-7xl mx-auto flex">
+      <LeftSidebar
+        username={user.username}
+        name={user.name}
+        imageUrl={user.imageUrl}
+      />
       {children}
       <RightSidebar />
     </main>
