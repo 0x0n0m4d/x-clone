52b62530d841a852a5737ba43a298b487332b450
d3m0n <th3l1ghtd3m0n@gmail.com>
2024-11-25 06:45:38 -0300

    feat(hooks): add tweet modal

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { links } from '@/constants';
+import { useTweetModal } from '@/hooks/useTweetModal';
 import { cn } from '@/lib/utils';
 import { Button } from './ui/button';
 
@@ -17,8 +18,9 @@ interface LeftSidebarProps {
 
 const LeftSidebar = ({ username, name, imageUrl }: LeftSidebarProps) => {
   const pathname = usePathname();
+  const openTweetModal = useTweetModal(state => state.onOpen);
   return (
-    <aside className="w-fit h-screen p-3 border-r border-r-gray-300 hidden sm:flex flex-col justify-between">
+    <aside className="w-fit h-screen p-3 border-r border-r-gray-300 max-sm:hidden sm:flex flex-col justify-between">
       <ul className="flex flex-col space-y-6">
         {links.map(link => {
           if (!link.href) link.href = `/${username}`;
@@ -30,7 +32,7 @@ const LeftSidebar = ({ username, name, imageUrl }: LeftSidebarProps) => {
             <li
               key={link.title}
               className={cn(
-                'w-fit p-3 lg:py-3 lg:px-5 rounded-full hover:bg-black-200 transition',
+                'w-fit max-lg:p-3 lg:py-3 lg:px-5 rounded-full hover:bg-black-200 transition',
                 isSamePath && 'bg-black-200 font-bold'
               )}
             >
@@ -46,23 +48,27 @@ const LeftSidebar = ({ username, name, imageUrl }: LeftSidebarProps) => {
                   className="object-contain"
                 />
                 {isNotLogo && (
-                  <span className="hidden lg:inline">{link.title}</span>
+                  <span className="max-lg:hidden lg:inline">{link.title}</span>
                 )}
               </Link>
             </li>
           );
         })}
-        <Button variant="primary" className="w-fit lg:w-full p-3">
-          <span className="hidden lg:inline">Tweet</span>
-          <span className="lg:hidden">
+        <Button
+          variant="primary"
+          className="max-lg:w-fit lg:w-full p-3"
+          onClick={openTweetModal}
+        >
+          <span className="max-lg:hidden lg:inline">Tweet</span>
+          <span className="max-lg:inline lg:hidden">
             <Plus size={30} />
           </span>
         </Button>
       </ul>
       <SignedIn>
         <SignOutButton>
-          <div className="p-3 lg:py-2 lg:px-5 rounded-full hover:bg-black-200 transition flex items-center gap-x-12 cursor-pointer">
-            <div className="hidden lg:flex items-center gap-x-6">
+          <div className="max-lg:p-3 lg:py-2 lg:px-5 rounded-full hover:bg-black-200 transition flex items-center gap-x-20 cursor-pointer">
+            <div className="max-lg:hidden lg:flex items-center gap-x-6">
               <Image
                 src={imageUrl}
                 alt={name}
@@ -72,8 +78,15 @@ const LeftSidebar = ({ username, name, imageUrl }: LeftSidebarProps) => {
                 className="object-contain rounded-full"
               />
               <div className="flex flex-col items-start">
-                <h5 className="font-bold text-white tracking-wide">{name}</h5>
-                <span className="text-gray-200 font-bold">@{username}</span>
+                <h5 className="font-bold text-white tracking-wide whitespace-nowrap">
+                  {name.length > 10 ? `${name.slice(0, 10)}...` : name}
+                </h5>
+                <span className="text-gray-200 font-bold whitespace-nowrap">
+                  @
+                  {username.length > 8
+                    ? `${username.slice(0, 8)}...`
+                    : username}
+                </span>
               </div>
             </div>
             <LogOut size={30} />
