90d99f5368ca983252121a0b16dd632d35d84f8e
d3m0n <th3l1ghtd3m0n@gmail.com>
2024-11-28 16:13:20 -0300

    fix(app): handle `isCompleted` bug

--- src/app/(auth)/onboarding/page.tsx
+++ src/app/(auth)/onboarding/page.tsx
@@ -1,4 +1,4 @@
-import { currentUser, User } from '@clerk/nextjs/server';
+import { currentUser } from '@clerk/nextjs/server';
 import Image from 'next/image';
 import { redirect } from 'next/navigation';
 import { getUserAction } from '@/actions/user.action';
@@ -6,7 +6,7 @@ import Logout from '@/components/Logout';
 import OnBoarding from '@/components/OnBoarding';
 
 const Page = async () => {
-  const clerkUser: User | null = await currentUser();
+  const clerkUser = await currentUser();
 
   if (!clerkUser) return null;
 
@@ -14,15 +14,17 @@ const Page = async () => {
     id: clerkUser.id,
     imageUrl: clerkUser.imageUrl,
     name: '',
-    username: clerkUser.username!,
+    username: clerkUser.username!.toLowerCase(),
     email: clerkUser.emailAddresses[0].emailAddress,
     bio: ''
   };
 
   const user = await getUserAction(clerkUser.id);
 
-  const isCompleted = user && user.isCompleted;
-  if (isCompleted) redirect('/home');
+  if (user && 'isCompleted' in user) {
+    const isCompleted = user.isCompleted;
+    if (isCompleted) redirect('/home');
+  }
 
   return (
     <section className="w-full h-full flex lg:flex-row max-lg:flex-col">
