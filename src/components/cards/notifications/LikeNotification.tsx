97aeb76c94b127833c23d39828aad391bea20190
d3m0n <th3l1ghtd3m0n@gmail.com>
2024-12-31 08:27:02 -0300

    feat(components): add notifications cards

---
+++ src/components/cards/notifications/LikeNotification.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { DataNotification } from '@/interfaces/notifications.interface';
+
+interface Props {
+  dataNotification: DataNotification;
+}
+
+const LikeNotification = ({ dataNotification }: Props) => {
+  return <div>Like Notifications</div>;
+};
+
+export default LikeNotification;
---
+++ src/components/cards/notifications/ReplyNotification.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { DataNotification } from '@/interfaces/notifications.interface';
+
+interface Props {
+  dataNotification: DataNotification;
+}
+
+const ReplyNotification = ({ dataNotification }: Props) => {
+  return <div>Reply Notifications</div>;
+};
+
+export default ReplyNotification;
