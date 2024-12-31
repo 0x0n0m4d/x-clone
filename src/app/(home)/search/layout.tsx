302c7bdd86e9934ce916f72b28fbd0d70df67dba
d3m0n <th3l1ghtd3m0n@gmail.com>
2024-12-30 22:17:19 -0300

    feat(app): implement search page

---
+++ src/app/(home)/search/layout.tsx
@@ -0,0 +1,27 @@
+import { ReactNode, Suspense } from 'react';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+import { getUserAction } from '@/actions/user.action';
+import Topbar from '@/components/search/Topbar';
+import Loading from '@/components/sharing/Loading';
+
+interface Props {
+  children: ReactNode;
+}
+
+const Layout = async ({ children }: Props) => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) return null;
+
+  const user = await getUserAction(clerkUser.id);
+  if (!user || 'message' in user) redirect('/');
+
+  return (
+    <>
+      <Topbar user={user} />
+      <Suspense fallback={<Loading />}>{children}</Suspense>
+    </>
+  );
+};
+
+export default Layout;
