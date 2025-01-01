77a24f7c911e173a1d125313e57bfd72d2359452
d3m0n <th3l1ghtd3m0n@gmail.com>
2025-01-01 15:25:13 -0300

    chore(lib): remove unused function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -46,20 +46,6 @@ export const formatDateTime = (Date: Date) => {
   return `${formattedTime} · ${formattedDate}`;
 };
 
-export const getErrorMessage = (error: unknown): string => {
-  let message: string;
-
-  if (error instanceof Error) {
-    message = error.message;
-  } else if (error && typeof error === 'object' && 'message' in error) {
-    message = String(error.message);
-  } else {
-    message = 'Something went wrong.';
-  }
-
-  return message;
-};
-
 export const toastOptions = {
   duration: 2000,
   style: {
