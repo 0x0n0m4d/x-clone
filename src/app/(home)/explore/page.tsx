464389b3544e41ac9304299bf42ebcdfae4970e8
d3m0n <th3l1ghtd3m0n@gmail.com>
2025-01-02 23:14:06 -0300

    feat(app): add pagination to explore page

--- src/app/(home)/explore/page.tsx
+++ src/app/(home)/explore/page.tsx
@@ -1,26 +1,58 @@
 import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 import { getUserAction, getUsersAction } from '@/actions/user.action';
-import ShowUserData from '@/components/explore/ShowUsersData';
+import Users from '@/components/cards/Users';
+import PaginationButtons from '@/components/sharing/PaginationButtons';
+import { isValidPage } from '@/lib/utils';
 
-const Page = async () => {
+interface Props {
+  searchParams: {
+    page: string;
+  };
+}
+
+const Page = async ({ searchParams }: Props) => {
+  const { page: qPage } = searchParams;
+  const page = isValidPage(qPage);
+
   const clerkUser = await currentUser();
   if (!clerkUser) return null;
 
   const user = await getUserAction(clerkUser.id);
   if (!user) redirect('/');
 
-  let users = await getUsersAction({
+  const users = await getUsersAction({
     userId: user.id,
-    size: 20
+    size: 30,
+    page
   });
 
-  if (!users?.length) users = [];
-
-  return (
+  return users?.data.length ? (
     <>
-      <ShowUserData initialDataUsers={users!} user={user} />
+      {users.data.map(dataUser => (
+        <Users
+          key={dataUser.id}
+          username={dataUser.username}
+          name={dataUser.name}
+          imageUrl={dataUser.imageUrl}
+          userId={dataUser.id}
+          currentUser={user}
+        />
+      ))}
+      <PaginationButtons
+        currentPage={page}
+        currentPath={'/explore'}
+        hasNext={users.hasNext}
+      />
     </>
+  ) : (
+    <div className="flex justify-center py-4 px-3">
+      <div className="flex flex-col items-start">
+        <p className="font-normal text-gray-200">
+          Ty searching for something else
+        </p>
+      </div>
+    </div>
   );
 };
 
