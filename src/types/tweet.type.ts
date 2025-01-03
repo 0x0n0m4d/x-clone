71f247198cc12c48da64468fd34466ae67018ea7
d3m0n <th3l1ghtd3m0n@gmail.com>
2025-01-03 04:52:07 -0300

    refactor(type): move `WhereFilter` to `tweet.type.ts`

--- src/types/tweet.type.ts
+++ src/types/tweet.type.ts
@@ -4,3 +4,13 @@ export type GetTweetsActionType = {
   data: DetailedTweet[];
   hasNext: boolean;
 };
+
+export type WhereFilter = {
+  parentId: string | null;
+  user: {
+    followers: { some: { followingId: string } | undefined };
+  };
+  bookmarks: { some: { userId: string } } | undefined;
+  likes: { some: { userId: string } } | undefined;
+  userId: string | undefined;
+};
